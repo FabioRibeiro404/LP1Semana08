bd46a16dae4e2d2abd2e9109c62b707767c6c252
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-04-26 14:08:31 +0100

    Add comparer for kind, value and description

---
+++ LootSort/Loot.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace LootSort
+{
+    /// <summary>
+    /// The Loot class should implement IComparable<Loot>
+    /// </summary>
+    public class Loot : IComparable<Loot>
+    {
+        /// <summary>Type of loot.</summary>
+        public LootType Kind { get; }
+
+        /// <summary>Loot description.</summary>
+        public string Description { get; }
+
+        /// <summary>Loot value.</summary>
+        public float Value { get; }
+
+        /// <summary>
+        /// Create a new instance of loot.
+        /// </summary>
+        /// <param name="kind">Type of loot.</param>
+        /// <param name="description">Loot description.</param>
+        /// <param name="value">Loot value.</param>
+        public Loot(LootType kind, string description, float value)
+        {
+            Kind = kind;
+            Description = description;
+            Value = value;
+        }/*
+        public int CompareTo(Loot type1, Loot type2)
+        {
+            int valueComp = type2.Value.CompareTo(type1.Value);
+
+            int kindComp = type1.Kind.CompareTo(type2.Kind);
+
+            int descriptionComp = type1.Description.CompareTo(type2.Description);
+
+            //if ()
+ 
+            return  kindComp + valueComp + descriptionComp;
+        }*/
+
+        
+        public int CompareTo(Loot other)
+        {
+            char kindSrt = Kind.ToString()[0];
+            char otherSrt = other.Kind.ToString()[0]; 
+
+            if (other == null) return 1;
+
+            if (kindSrt > otherSrt) return 1;
+
+            else if (kindSrt < otherSrt) return -1;
+
+            else return 0;
+        }
+        
+
+        /// <summary>
+        /// Return a nicely formatted string representing the loot instance.
+        /// </summary>
+        /// <returns>
+        /// A nicely formatted string representing the loot instance.
+        /// </returns>
+        public override string ToString() =>
+            $"[{Kind,15}]\t{Value:f2}\t{Description}";
+
+    }
+}
